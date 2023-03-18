95edf3ee49af09ebeefb5fb5792d09770bf14efa
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2023-03-18 15:41:24 +0100

    clients

--- src/CommentList.js
+++ src/CommentList.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    const { status } = comment;
+    if (status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    if (status === "pending") {
+      content = "This comment is pending";
+    }
+    if (status === "approved") {
+      content = comment.content;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
